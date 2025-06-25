d5dcb2a03ac854d08eef289003f6dfc8cce1fdee
SeungBeen Wee <WinningBean@gmail.com>
2025-06-25 17:15:10 +0900

    init: 프로젝트 세팅

---
+++ bin/dart_console_shop.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_shop/dart_console_shop.dart' as dart_console_shop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_shop.calculate()}!');
+}
---
+++ test/dart_console_shop_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_shop/dart_console_shop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
