46a1a337ef38eda427b636558118d33cb5a30b1c
SeungBeen Wee <WinningBean@gmail.com>
2025-06-26 11:25:21 +0900

    test: 기본 동작 테스트

--- test/dart_console_shop_test.dart
+++ test/dart_console_shop_test.dart
@@ -1,3 +1,34 @@
-void main() {
+import 'package:dart_console_shop/product.dart';
+import 'package:dart_console_shop/shopping_mall.dart';
+import 'package:test/test.dart';
 
+void main() {
+  test('shopping', () {
+    var products = [
+      Product('Apple', 1000),
+      Product('Banana', 5000),
+      Product('Orange', 7000),
+    ];
+    
+    var mall = ShoppingMall(products);
+    
+    // Show products
+    mall.showProducts();
+    
+    // Add products to cart
+    mall.addToCart(products[0]); // Apple
+    mall.addToCart(products[1]); // Banana
+    
+    // Show total price
+    mall.showTotal();
+    
+    // Check if cart contains the correct products and total price
+    expect(mall.cart.length, 2);
+    expect(mall.cartPrice, 6000);
+    
+    // Add another product to cart
+    mall.addToCart(products[2]); // Orange
+    expect(mall.cart.length, 3);
+    expect(mall.cartPrice, 13000);
+  });
 }
